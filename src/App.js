a50a2fb5d06b223ba1e0c9384cd7d8b783d37999
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-08-09 16:31:57 -0500

    restaurar de clase 3

--- src/App.js
+++ src/App.js
@@ -2,27 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let nombre = 'Roberto';
-  let conectado = true;
-  let estaciones = ['Primavera','Verano','Otoño','Invierno'];
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor='nombre'>Nombre:</label>
-        <input type="text" id="nombre"></input>
-        <h1>{nombre}</h1>
-        <p>
-          {conectado? "El usuario está logeado" : "El usuario está desconectado"}
-        </p>
         <p>
           Editar <code>src/App.js</code> and save to reload.
         </p>
-        <ul>
-          {estaciones.map((elemento) =>(
-            <li>{elemento}</li>
-          ))}
-        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
